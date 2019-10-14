b8fd2011d0ffb743862710d135c4d819aecfc050
jkrivonos <ja.krivonos@gmail.com>
2019-10-14 16:59:31 +0300

    валидация формы регистрации

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import router from './router'
 import store from 'vuex'
@@ -7,6 +8,7 @@ import dateFilter from './filters/date.filter'
 
 Vue.config.productionTip = false;
 Vue.use(router);
+Vue.use(Vuelidate);
 
 Vue.filter('date', dateFilter);
 new Vue({
